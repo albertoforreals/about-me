4ac9235210ce97fbb3e9cf145825b5bfd7e5d439
alberto <albertosprojects123@gmail.com>
2025-02-06 23:35:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-const canvas = document.getElementById('interactive-bg');
-const ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particlesArray = [];
-const numberOfParticles = 100;
-
-class Particle {
-    constructor(x, y, size, color, weight) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.color = color;
-        this.weight = weight;
-    }
-
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-
-    update() {
-        this.size -= 0.05;
-        if (this.size < 0) {
-            this.x = (Math.random() * canvas.width) + canvas.width;
-            this.y = (Math.random() * canvas.height) + canvas.height;
-            this.size = (Math.random() * 5) + 2;
-            this.weight = (Math.random() * 2) - 0.5;
-        }
-        this.y += this.weight;
-        this.weight += 0.05;
-
-        if (this.y > canvas.height -
+	setInterval(function() {
+            let randomAngle = Math.floor(Math.random() * 360);
+            document.body.style.background = `linear-gradient(${randomAngle}deg, black, white)`;
+        }, 2000);
